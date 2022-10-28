4254d71aa12e30c07ca7c83177eedc8c3581bc48
shashank mandava <shashankmandava@gmail.com>
2022-10-27 22:33:40 -0700

    First Commit

---
+++ src/com/GoodString.java
@@ -0,0 +1,9 @@
+
+package com.example.goodString;
+public class GoodString {
+    static Boolean goodString(){
+        System.out.println("Not Implemented");
+        return true;
+    }
+    public static void main(String[] args){ goodString();}
+}
